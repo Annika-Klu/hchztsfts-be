830745e0263aa0dfda15e395d5785a77f25c8df8
Klüpfel Annika <AnnikaKluepfel@web.de>
2023-07-19 03:00:10 +0200

    add auth route(r)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-
+import authRouter from "./routes/authRouter.js";
 import dataRouter from "./routes/dataRouter.js";
 
 const app = express();
@@ -9,6 +9,7 @@ const port = process.env.PORT || 7777;
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRouter);
 app.use("/data", dataRouter);
 
 app.listen(port, () => {
